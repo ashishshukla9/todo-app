858709f529f9066a887c57eb521f6553cf6d448a
Ashish Shukla <shuklaashish655@gmail.com>
2025-05-28 18:20:36 +0530

    Updates

---
+++ precache-manifest.03142cea7d5147787324c6e09fbc949c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba7a17e8f63b86eb5eca1acc9d5aac2",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "be443073090f471066aa",
+    "url": "/todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/todo-app/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/todo-app/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "be443073090f471066aa",
+    "url": "/todo-app/static/js/main.903cece1.chunk.js"
+  },
+  {
+    "revision": "c01e25032b6cc4a6bf48",
+    "url": "/todo-app/static/js/runtime~main.b929bfaa.js"
+  }
+]);
